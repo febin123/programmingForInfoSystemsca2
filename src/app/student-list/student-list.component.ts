0e55d81b994e37a0fd28385f4d64bd3923c43c61
febin123 <febsep96@gmail.com>
2022-12-13 21:13:42 +0000

    created table and added data through student-list-component

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-student-list',
@@ -7,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit{
 
+
+  students:Student[];
   constructor(){}
   ngOnInit(): void {
-      
+      this.students=[{
+        "studentId":1,
+        "firstName":"febin",
+        "lastName":"babu",
+        "emailId":"febse@gmail.com",
+        "studentAge":12,
+        "contactNumber":112334
+      }]
   }
 }
